fa30ce786c1fd1703eccb120742a583af379265e
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2025-04-04 13:38:27 +0200

    Creation of setup.sh, with only, for now, easy symbolic linking to input matrices

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# some miscellaneous extra steps to get everything up and ready. Uncomment
+# segments as needed
+
+# # this is for getting access to the input matrices .. replace the
+# # first two lines with your actual paths
+# ROOT_OF_LibNEGF=.
+# rm -Rf $ROOT_OF_LibNEGF/matrices/
+# PATH_TO_BUILT_MATRICES=/Users/ramirez/Documents/projects/libNEGF/matrices/3x3/built_matrices/
+# mkdir $ROOT_OF_LibNEGF/matrices/
+# PATH_TO_SYMB_LINK_DIR=$ROOT_OF_LibNEGF/matrices/3x3/
+# mkdir $PATH_TO_SYMB_LINK_DIR
+# ln -s $PATH_TO_BUILT_MATRICES $PATH_TO_SYMB_LINK_DIR
