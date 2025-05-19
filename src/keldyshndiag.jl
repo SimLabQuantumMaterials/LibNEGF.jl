d67aa3ab073a5b27dc4e40addb86633aaa61e7e3
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2025-05-19 09:06:49 +0200

    Pack GEMM for the BlockMatrix type

--- src/keldyshndiag.jl
+++ src/keldyshndiag.jl
@@ -43,7 +43,7 @@ function keldyshndiag_v1!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::
 end
 
 # tB can be either 'C' (for adjoint) or 'N' for no adjoint
-function keldysh_gemm!(tB::Char, C_::BlockMatrix, A_::BlockMatrix, B_::BlockMatrix, ix::Int, jx::Int)
+function bm_local_gemm!(tA::Char, tB::Char, alpha::Number, A_::BlockMatrix, B_::BlockMatrix, beta::Number, C_::BlockMatrix, ix::Int, jx::Int)
     npl = size(A_.blockSizes)[1]
     nUpDiagA = Int((A_.ndiag["in"] - 1) / 2)
     nUpDiagB = Int((B_.ndiag["in"] - 1) / 2)
@@ -53,7 +53,9 @@ function keldysh_gemm!(tB::Char, C_::BlockMatrix, A_::BlockMatrix, B_::BlockMatr
     A = A_.M
     B = B_.M
 
-    be_fill!(C[ix, jx], convert(A_.nrsType, 0.0))
+    if beta == convert(nrsType, 0.0)
+        be_fill!(C[ix, jx], convert(A_.nrsType, 0.0))
+    end
 
     for kx = 1:npl
         # avoid accessing undefs in A and B
@@ -61,10 +63,35 @@ function keldysh_gemm!(tB::Char, C_::BlockMatrix, A_::BlockMatrix, B_::BlockMatr
         cond2 = (jx <= kx + nUpDiagB) && (jx >= kx - nUpDiagB)
 
         if cond1 && cond2
+            # do the transposition by hand
             if tB == 'C'
-                be_gemm!('N', 'C', convert(nrsType, 1.0), A[ix, kx], B[jx, kx], convert(nrsType, 1.0), C[ix, jx])
+                be_gemm!('N', 'C', alpha, A[ix, kx], B[jx, kx], convert(nrsType, 1.0), C[ix, jx])
             else
-                be_gemm!('N', 'N', convert(nrsType, 1.0), A[ix, kx], B[kx, jx], convert(nrsType, 1.0), C[ix, jx])
+                be_gemm!('N', 'N', alpha, A[ix, kx], B[kx, jx], convert(nrsType, 1.0), C[ix, jx])
+            end
+        end
+    end
+end
+
+function bm_gemm!(tA::Char, tB::Char, alpha::Number, A::BlockMatrix, B::BlockMatrix, beta::Number, C::BlockMatrix)
+    ndiag = C.ndiag
+    npl = size(B.blockSizes)[1]
+
+    # loop over the block sizes, conversely over the block rows
+    for ix = 1:npl
+        # now, copy the blocks within the ix-th row
+        # left
+        if ix > 1
+            for jx = (ix-1):-1:max(1, ix - Int((ndiag["out"] - 1) / 2))
+                bm_local_gemm!(tA, tB, alpha, A, B, beta, C, ix, jx)
+            end
+        end
+        # center
+        bm_local_gemm!(tA, tB, alpha, A, B, beta, C, ix, ix)
+        # right
+        if ix < npl
+            for jx = (ix+1):1:min(npl, ix + Int((ndiag["out"] - 1) / 2))
+                bm_local_gemm!(tA, tB, alpha, A, B, beta, C, ix, jx)
             end
         end
     end
@@ -74,53 +101,11 @@ end
 function keldyshndiag_v2!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData)
     bndiag_of_inv_ddrgf!(Binv, B, auxData.auxDataRGF, td)
 
-    npl = size(B.blockSizes)[1]
-
     # the (block) indices ix and jx are running over auxData.bmLargeBuff
 
-    # FIRST, do auxData.bmLargeBuff = Binv * A'
+    # do auxData.bmLargeBuff = Binv * A'
+    bm_gemm!('N', 'C', convert(Binv.nrsType, 1.0), Binv, A, convert(Binv.nrsType, 0.0), auxData.bmLargeBuff)
 
-    ndiag = auxData.bmLargeBuff.ndiag
-
-    # loop over the block sizes, conversely over the block rows
-    for ix = 1:npl
-        # now, copy the blocks within the ix-th row
-        # left
-        if ix > 1
-            for jx = (ix-1):-1:max(1, ix - Int((ndiag["out"] - 1) / 2))
-                keldysh_gemm!('C', auxData.bmLargeBuff, Binv, A, ix, jx)
-            end
-        end
-        # center
-        keldysh_gemm!('C', auxData.bmLargeBuff, Binv, A, ix, ix)
-        # right
-        if ix < npl
-            for jx = (ix+1):1:min(npl, ix + Int((ndiag["out"] - 1) / 2))
-                keldysh_gemm!('C', auxData.bmLargeBuff, Binv, A, ix, jx)
-            end
-        end
-    end
-
-    # THEN, do C = A * auxData.bmLargeBuff
-
-    ndiag = C.ndiag
-
-    # loop over the block sizes, conversely over the block rows
-    for ix = 1:npl
-        # now, copy the blocks within the ix-th row
-        # left
-        if ix > 1
-            for jx = (ix-1):-1:max(1, ix - Int((ndiag["out"] - 1) / 2))
-                keldysh_gemm!('N', C, A, auxData.bmLargeBuff, ix, jx)
-            end
-        end
-        # center
-        keldysh_gemm!('N', C, A, auxData.bmLargeBuff, ix, ix)
-        # right
-        if ix < npl
-            for jx = (ix+1):1:min(npl, ix + Int((ndiag["out"] - 1) / 2))
-                keldysh_gemm!('N', C, A, auxData.bmLargeBuff, ix, jx)
-            end
-        end
-    end
+    # do C = A * auxData.bmLargeBuff
+    bm_gemm!('N', 'N', convert(A.nrsType, 1.0), A, auxData.bmLargeBuff, convert(Binv.nrsType, 0.0), C)
 end
