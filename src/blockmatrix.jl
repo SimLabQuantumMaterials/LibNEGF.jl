bdedfd6654ee4a21d502d05ea0d1e90b00618ca0
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2025-05-19 09:15:28 +0200

    Move BlockMatrix's GEMM to blockmatrix.jl

--- src/blockmatrix.jl
+++ src/blockmatrix.jl
@@ -312,3 +312,58 @@ function bm_blocks_define_identity!(M::BlockMatrix)
         A.M[ix, ix] = be_identity(A.nrsType, iend - ibeg + 1)
     end
 end
+
+# tB can be either 'C' (for adjoint) or 'N' for no adjoint
+function bm_local_gemm!(tA::Char, tB::Char, alpha::Number, A_::BlockMatrix, B_::BlockMatrix, beta::Number, C_::BlockMatrix, ix::Int, jx::Int)
+    npl = size(A_.blockSizes)[1]
+    nUpDiagA = Int((A_.ndiag["in"] - 1) / 2)
+    nUpDiagB = Int((B_.ndiag["in"] - 1) / 2)
+    nrsType = A_.nrsType
+
+    C = C_.M
+    A = A_.M
+    B = B_.M
+
+    if beta == convert(nrsType, 0.0)
+        be_fill!(C[ix, jx], convert(A_.nrsType, 0.0))
+    end
+
+    for kx = 1:npl
+        # avoid accessing undefs in A and B
+        cond1 = (kx <= ix + nUpDiagA) && (kx >= ix - nUpDiagA)
+        cond2 = (jx <= kx + nUpDiagB) && (jx >= kx - nUpDiagB)
+
+        if cond1 && cond2
+            # do the transposition by hand
+            if tB == 'C'
+                be_gemm!('N', 'C', alpha, A[ix, kx], B[jx, kx], convert(nrsType, 1.0), C[ix, jx])
+            else
+                be_gemm!('N', 'N', alpha, A[ix, kx], B[kx, jx], convert(nrsType, 1.0), C[ix, jx])
+            end
+        end
+    end
+end
+
+function bm_gemm!(tA::Char, tB::Char, alpha::Number, A::BlockMatrix, B::BlockMatrix, beta::Number, C::BlockMatrix)
+    ndiag = C.ndiag
+    npl = size(B.blockSizes)[1]
+
+    # loop over the block sizes, conversely over the block rows
+    for ix = 1:npl
+        # now, copy the blocks within the ix-th row
+        # left
+        if ix > 1
+            for jx = (ix-1):-1:max(1, ix - Int((ndiag["out"] - 1) / 2))
+                bm_local_gemm!(tA, tB, alpha, A, B, beta, C, ix, jx)
+            end
+        end
+        # center
+        bm_local_gemm!(tA, tB, alpha, A, B, beta, C, ix, ix)
+        # right
+        if ix < npl
+            for jx = (ix+1):1:min(npl, ix + Int((ndiag["out"] - 1) / 2))
+                bm_local_gemm!(tA, tB, alpha, A, B, beta, C, ix, jx)
+            end
+        end
+    end
+end
