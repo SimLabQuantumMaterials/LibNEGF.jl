54bc528aabdeee3f62960c484907c00c2362c87b
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2025-11-12 16:37:33 +0100

    Include the upward pass of RGF within RKD

--- src/keldyshndiag.jl
+++ src/keldyshndiag.jl
@@ -6,28 +6,40 @@ RGF operations, and `bmLargeBuff` to do the further GEMM.
 """
 struct AuxDataKeldysh
     auxDataRGF::AuxDataRGF
-    bmLargeBuff::BlockMatrix
+    buffS::BlockMatrix
+    buffMdiag::BlockMatrix
 end
 
-function allocate_aux_data_Keldysh(M::BlockMatrix, auxDataRGF::AuxDataRGF)::AuxDataKeldysh
-    npl = size(M.blockSizes)[1]
-    # the number of block diagonals
-    m = 1 + 2 * (M.ndiag["in"] - 1)
+function allocate_aux_data_Keldysh(M::BlockMatrix, S::BlockMatrix, nrBLASThreadsOuter::Int,
+    nrBLASThreadsInner)::AuxDataKeldysh
+    # npl = size(M.blockSizes)[1]
 
-    # in general, these type of auxiliary block matrices will contain
-    # Array-like object and not LU-like, as specified by the last param
-    bmLargeBuff = BlockMatrix(copy(M.blockSizes), ArrayOrLU_(undef, npl, npl),
-        Dict("in" => m, "out" => m), M.nrsType, 0, )
-    bm_blocks_define!(bmLargeBuff, 1)
+    auxDataRGF = allocate_aux_data_RGF(M, nrBLASThreadsOuter, nrBLASThreadsInner)
+
+    # # the number of block diagonals
+    # m = 1 + 2 * (M.ndiag["in"] - 1)
+
+    # # in general, these type of auxiliary block matrices will contain
+    # # Array-like object and not LU-like, as specified by the last param
+    # bmLargeBuff = BlockMatrix(copy(M.blockSizes), ArrayOrLU_(undef, npl, npl),
+    #     Dict("in" => m, "out" => m), M.nrsType, 0, )
+    # bm_blocks_define!(bmLargeBuff, 1)
+
+    # this is the main buffer, of the same structure as the central operator in Keldysh, i.e., S
+    buffS = bm_copy(S)
+    # in principle, we only need the lower triangular part of the following buffer
+    buffMdiag = bm_copy(M)
 
     # the final struct with the buffers
-    auxDataKeldysh = AuxDataKeldysh(auxDataRGF, bmLargeBuff)
+    # auxDataKeldysh = AuxDataKeldysh(auxDataRGF, bmLargeBuff)
+    auxDataKeldysh = AuxDataKeldysh(auxDataRGF, buffS, buffMdiag)
 
     return auxDataKeldysh
 end
 
 """
-    keldyshndiag!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData, vsn::String)
+    keldyshndiag!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, 
+        td::TimingData, vsn::String)
 
 Computes C = A * Binv * A^{H}, where Binv is the block tridiagonal of
 
@@ -36,18 +48,22 @@ Computes C = A * Binv * A^{H}, where Binv is the block tridiagonal of
 level but further within Keldysh and RGF.
 - `vsn:String`: the version of the implementation, currently available "v1" and "v2".
 """
-function keldyshndiag!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData, cd::CountingData, vsn::String)
-    if vsn == "v1"
-        keldyshndiag_v1!(C, Binv, B, A, auxData, td, cd)
-    elseif vsn == "v2"
-        keldyshndiag_v2!(C, Binv, B, A, auxData, td, cd)
-    else
-        println("ERROR: Keldysh implementation version not available")
-    end
+function keldyshndiag!(M::BlockMatrix, S::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData, 
+    cd::CountingData)
+    keldyshndiag_v3!(M, S, auxData, td, cd)
+
+    # if vsn == "v1"
+    #     keldyshndiag_v1!(C, Binv, B, A, auxData, td, cd)
+    # elseif vsn == "v2"
+    #     keldyshndiag_v2!(C, Binv, B, A, auxData, td, cd)
+    # else
+    #     println("ERROR: Keldysh implementation version not available")
+    # end
 end
 
 # first version, naive, inefficient
-function keldyshndiag_v1!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData, cd::CountingData)
+function keldyshndiag_v1!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, 
+    td::TimingData, cd::CountingData)
     bndiag_of_inv_rgf_local!(Binv, B, auxData.auxDataRGF, td, cd)
 
     Binvsp = bm_convert(Binv)
@@ -62,7 +78,8 @@ function keldyshndiag_v1!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::
 end
 
 # a more efficient version
-function keldyshndiag_v2!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData, cd::CountingData)
+function keldyshndiag_v2!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::BlockMatrix, auxData::AuxDataKeldysh,
+    td::TimingData, cd::CountingData)
     bndiag_of_inv_rgf_local!(Binv, B, auxData.auxDataRGF, td, cd)
 
     # the (block) indices ix and jx are running over auxData.bmLargeBuff
@@ -72,4 +89,59 @@ function keldyshndiag_v2!(C::BlockMatrix, Binv::BlockMatrix, B::BlockMatrix, A::
 
     # do C = Binv * auxData.bmLargeBuff
     bm_gemm!('N', 'N', convert(A.nrsType, 1.0), Binv, auxData.bmLargeBuff, convert(Binv.nrsType, 0.0), C, td, cd)
+end
+
+# implementation of RKD
+# M : the T matrix in the case of block tridiagonal
+# S : the central operator in Keldysh
+function keldyshndiag_v3!(M::BlockMatrix, S::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData,
+    cd::CountingData)
+    # we first run the upward pass of RGF, before any RKD steps
+    keldyshndiag_upward_rgf!(M, auxData, td, cd)
+
+    # IMPORTANT : the factors of the form T_{..}t_{..} and t_{..}T_{..} are already pre-computed
+    #             and available in the block off-diagonal part of auxData.auxDataRGF.buffM
+
+    # -------------
+
+    # now we start with the actual RKD stuff
+
+    # TODO #1 : upward pass of RKD
+
+    # TODO #2 : central (upward/downward) pass of RKD
+
+    # TODO #3 : downward pass of RKD
+end
+
+# upward RGF pass only
+function keldyshndiag_upward_rgf!(M::BlockMatrix, auxData::AuxDataKeldysh, td::TimingData, cd::CountingData)
+    minusOneCmplx = convert(M.nrsType, -1.0)
+    plusOneCmplx = convert(M.nrsType, 1.0)
+    npl = size(M.blockSizes)[1]
+
+    buffM1 = auxData.auxDataRGF.buffM
+    # auxData.buffMdiag is block-diagonal
+    buffM2 = auxData.buffMdiag
+
+    # bottom element
+    be_lu!(buffM1.M[npl, npl], M.M[npl, npl], td, cd)
+
+    # middle elements
+    for ix = npl-1:-1:1
+        # first run mrdivide, to make use of the mldivide data as a buffer for mrdivide
+
+        # this is how we implement be_mrdivide!(..) via be_mldivide!(..)
+        begin
+            be_ctranspose!(buffM1.M[ix+1, ix], M.M[ix, ix+1], td, cd)
+            be_mldivide!('C', buffM2.M[ix+1, ix], buffM1.M[ix+1, ix], buffM1.M[ix+1, ix+1], td, cd)
+            be_ctranspose!(buffM1.M[ix, ix+1], buffM2.M[ix+1, ix], td, cd)
+        end
+
+        be_mldivide!('N', buffM1.M[ix+1, ix], M.M[ix+1, ix], buffM1.M[ix+1, ix+1], td, cd)
+
+        be_copy_in_hw!(buffM2.M[ix, ix], M.M[ix, ix])
+        be_gemm!('N', 'N', minusOneCmplx, M.M[ix, ix+1], buffM1.M[ix+1, ix], plusOneCmplx, buffM2.M[ix, ix], td, cd)
+        # TODO : double-check, but this last LU factorization seems to not be needed
+        be_lu!(buffM1.M[ix, ix], buffM2.M[ix, ix], td, cd)
+    end
 end
--- test/test_keldyshndiag.jl
+++ test/test_keldyshndiag.jl
@@ -40,67 +40,60 @@ blockSize = 32
                         # crate synthetic matrix
                         MbmSynth = bm_create_synthetic(MbmFromData, npl, blockSize)
                     else
-                        MbmSynth = bm_create_synthetic_random(npl, blockSize, precx)
+                        MbmSynth = bm_create_synthetic_random(npl, blockSize, precx, false)
                     end
 
                     # this is T, whose inverse is Gr
                     Mbm = MbmSynth
 
-                    # make Arandbm Hermitian
+                    # make Arandbm Hermitian, this will be the middle operator in Keldysh
                     Arandbm = bm_similar(Mbm, 2)
                     Arandsp = bm_convert(Arandbm)
                     Arandsp = (Arandsp + Arandsp') / 2
-                    Arandbm = bm_convert(Arandsp, Arandbm.blockSizes, Arandbm.ndiag)
+                    # BUT : we need to save Arandbm efficiently, considering that it is Hermitian,
+                    #       specified via the last parameter in the following line
+                    Arandbm = bm_convert(Arandsp, Arandbm.blockSizes, Arandbm.ndiag, true)
 
-                    # BUT : we need to save Arandbm efficiently, considering that it is Hermitian
-                    # TODO
+                    # we call this Sbm, which stands for \Sigma^{n}
+                    Sbm = Arandbm
 
-                    Al = Arandbm.M[1,1]
-                    Alt = LinearAlgebra.UpperTriangular(Al)
+                    # FIRST, do Keldysh via the recursive algorithm
 
-                    println(sizeof(Al))
-                    println(sizeof(Alt))
-
-                    Al[1,1] = 17.893
-                    println(Alt[1,1])
-
-                    # we call this Sn, which stands for \Sigma^{n}
-                    Sn = Arandbm
+                    # pre-allocate buffer data for RKD
+                    auxDataKeldysh = allocate_aux_data_Keldysh(Mbm, Sbm, parse(Int, ARGS[2]), parse(Int, ARGS[3]))
+                    keldyshndiag!(Mbm, Sbm, auxDataKeldysh, TimingData(), CountingData())
 
                     exit()
 
-                    # FIRST, do Keldysh 'by hand'
+                    # # FIRST, do Keldysh 'by hand'
 
-                    # pre-allocate buffer data for DD-RGF
-                    auxDataRGF = allocate_aux_data_RGF(Mbm)
+                    # # # pre-allocate the output matrix
+                    # # MbmInvNdiag = bm_similar(Mbm, 1)
+                    # # # get the block n-diagonal of M^-1 via RGF
+                    # # bndiag_of_inv_ddrgf!(MbmInvNdiag, Mbm, auxDataRGF, TimingData(), CountingData())
 
-                    # # pre-allocate the output matrix
-                    # MbmInvNdiag = bm_similar(Mbm, 1)
-                    # # get the block n-diagonal of M^-1 via RGF
-                    # bndiag_of_inv_ddrgf!(MbmInvNdiag, Mbm, auxDataRGF, TimingData(), CountingData())
+                    # # convert back to sparse
+                    # MinvSp = bm_convert(MbmInvNdiag)
 
-                    # convert back to sparse
-                    MinvSp = bm_convert(MbmInvNdiag)
+                    # C1sp = MinvSp * (Arandsp * MinvSp')
+                    # # but, we need to extract the bndiag part of C1sp
+                    # C1bm = bm_convert(C1sp, Mbm.blockSizes, Mbm.ndiag)
+                    # C1sp = bm_convert(C1bm)
+                    # # for de-allocation of some matrices
+                    # MinvSp = 0
+                    # Arandsp = 0
+                    # C1bm = 0
+                    # GC.gc()
 
-                    C1sp = MinvSp * (Arandsp * MinvSp')
-                    # but, we need to extract the bndiag part of C1sp
-                    C1bm = bm_convert(C1sp, Mbm.blockSizes, Mbm.ndiag)
-                    C1sp = bm_convert(C1bm)
-                    # for de-allocation of some matrices
-                    MinvSp = 0
-                    Arandsp = 0
-                    C1bm = 0
-                    GC.gc()
+                    # # THEN, do Keldysh via its function
 
-                    # THEN, do Keldysh via its function
+                    # C2bm = bm_similar(Mbm, 1)
+                    # auxDataKeldysh = allocate_aux_data_Keldysh(Mbm, auxDataRGF)
+                    # keldyshndiag!(C2bm, MbmInvNdiag, Mbm, Arandbm, auxDataKeldysh, TimingData(), CountingData(), "v2")
+                    # C2sp = bm_convert(C2bm)
 
-                    C2bm = bm_similar(Mbm, 1)
-                    auxDataKeldysh = allocate_aux_data_Keldysh(Mbm, auxDataRGF)
-                    keldyshndiag!(C2bm, MbmInvNdiag, Mbm, Arandbm, auxDataKeldysh, TimingData(), CountingData(), "v2")
-                    C2sp = bm_convert(C2bm)
-
-                    relErr = LinearAlgebra.norm(Array(C1sp - C2sp), 2) / LinearAlgebra.norm(Array(C1sp), 2)
-                    @test relErr < roundoffs[precx] * 1.0E02
+                    # relErr = LinearAlgebra.norm(Array(C1sp - C2sp), 2) / LinearAlgebra.norm(Array(C1sp), 2)
+                    # @test relErr < roundoffs[precx] * 1.0E02
                 end
             end
         end
