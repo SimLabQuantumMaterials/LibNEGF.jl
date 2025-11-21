870c8ea6ffbe0892495f6c09c8df6889dd72002e
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2025-11-21 07:24:19 +0100

    Add DDRGF in the examples/ dir

--- examples/runexamples.jl
+++ examples/runexamples.jl
@@ -8,5 +8,13 @@ begin
     Pkg.instantiate()
 end
 
-# sequential RGF example
-include("example_rgf.jl")
+# choose one of "rgf", "ddrgf", "keldysh"
+whichExample = "ddrgf"
+
+if whichExample == "rgf"
+    # sequential RGF example
+    include("example_rgf.jl")
+elseif whichExample == "ddrgf"
+    include("example_ddrgf.jl")
+elseif whichExample == "keldysh"
+end
--- src/exports_example.jl
+++ src/exports_example.jl
@@ -4,3 +4,8 @@ export allocate_aux_data_RGF
 export bm_copy
 export bndiag_of_inv_rgf_local!
 export bm_convert
+
+# for DDRGF
+export allocate_aux_data_DDRGF
+export bndiag_of_inv_ddrgf!
+export bm_copy!
