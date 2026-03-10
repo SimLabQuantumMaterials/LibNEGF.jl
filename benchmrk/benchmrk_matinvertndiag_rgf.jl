3beb50e00f08190bb5adc50f3702b91975105cbd
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2026-03-10 09:23:28 +0100

    Fix benchmarks to re-enable RGF and RKD

--- benchmrk/benchmrk_matinvertndiag_rgf.jl
+++ benchmrk/benchmrk_matinvertndiag_rgf.jl
@@ -1,123 +1,129 @@
 Printf.@printf("Benchmarking bndiag_of_inv_ddrgf!(...)\n")
 
-LinearAlgebra.BLAS.set_num_threads(Int(parse(Float64, ARGS[4])))
+#LinearAlgebra.BLAS.set_num_threads(Int(parse(Float64, ARGS[4])))
 
+# TODO : restore the following commented block if we want to go back
+# to using whereFrom = 1
 # first, check if the number of threads divides the number of energy points,
 # exit if it doesn't
-if mod(nrEPoints, Threads.nthreads()) != 0
-    Printf.@printf("ERROR: the number of Julia threads (%d) does not divide the number \
-                    of energy points (%d)\n", Threads.nthreads(), nrEPoints)
-    exit()
+#if mod(nrEPoints, Threads.nthreads()) != 0
+#    Printf.@printf("ERROR: the number of Julia threads (%d) does not divide the number \
+#                    of energy points (%d)\n", Threads.nthreads(), nrEPoints)
+#    exit()
+#end
+
+# NOTE : RGF should be run with one Julia thread, and possible multiple
+# BLAS threads
+
+if Threads.nthreads() != 1
+    error("The number of Julia threads when running RGF must be 1")
 end
 
-for systemx in systemNames
-    for precx in precs
-        # check if there's enough memory for the allocations
-        check_if_enough_mem_rgf(npl, blockSize, precx)
+for precx in precs
+    # # check if there's enough memory for the allocations
+    # check_if_enough_mem_rgf(npl, blockSize, precx)
 
-        # create a flops and mems counter for each precision and thread
-        counters = Vector{CountingData}()
-        for ix = 1:Threads.nthreads()
-            if useFinerTimings == 1
-                push!(counters, CountingData(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
-            else
-                push!(counters, CountingData())
-            end
-        end
-        # create array of timers
-        timers = Vector{TimerOutput}()
-        for ix = 1:Threads.nthreads()
-            push!(timers, TimerOutput())
-        end
-        timerTagGlobal = "bndiag_of_inv_ddrgf_" * string(precx)
+    # create a flops and mems counter for each precision
+    counters = Vector{CountingData}()
+    if useFinerTimings == 1
+        push!(counters, CountingData(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+    else
+        push!(counters, CountingData())
+    end
+    # create array of timers
+    timers = Vector{TimerOutput}()
+    push!(timers, TimerOutput())
+    timerTagGlobal = "bndiag_of_inv_ddrgf_" * string(precx)
 
-        for k in kpoints
-            @timeit to timerTagGlobal begin
-                # loop over bunches of energy points
-                nrEgroups::Int = size(Epoints)[1] / Threads.nthreads()
-                for iEG = 1:nrEgroups
-                    # preallocate large data per thread
-                    Mins = Vector{BlockMatrix}()
-                    Mouts = Vector{BlockMatrix}()
-                    for ix = 1:Threads.nthreads()
-                        if whereFrom == 1
-                            iE = ix + (iEG - 1) * Threads.nthreads()
-                            # load matrices and build M
-                            listMatsToLoad = ["H", "S", "Sc"]
-                            loadedMats, blockSizes = load_matrices(systemx, Epoints[iE], k,
-                                listMatsToLoad, precx, whereFrom)
-                            H = loadedMats[1]
-                            S = loadedMats[2]
-                            Se = loadedMats[3]
-                            Msp = build_M_from_HS(H, S, Se, energVals[Epoints[iE]])
-                            Min = bm_convert(Msp, blockSizes, Dict("in" => 3, "out" => 3), false)
-                        else
-                            Min = bm_create_synthetic_random(npl, blockSize, precx, false)
-                        end
+    for k in kpoints
+        @timeit to timerTagGlobal begin
+            # loop over bunches of energy points - fixed to 1 for now
+            nrEgroups::Int = 1
+            for iEG = 1:nrEgroups
+                # preallocate large data per thread
+                Mins = Vector{BlockMatrix}()
+                Mouts = Vector{BlockMatrix}()
+
+                # if whereFrom == 1
+                #     iE = ix + (iEG - 1) * Threads.nthreads()
+                #     # load matrices and build M
+                #     listMatsToLoad = ["H", "S", "Sc"]
+                #     loadedMats, blockSizes = load_matrices(systemx, Epoints[iE], k,
+                #         listMatsToLoad, precx, whereFrom)
+                #     H = loadedMats[1]
+                #     S = loadedMats[2]
+                #     Se = loadedMats[3]
+                #     Msp = build_M_from_HS(H, S, Se, energVals[Epoints[iE]])
+                #     Min = bm_convert(Msp, blockSizes, Dict("in" => 3, "out" => 3), false)
+                # else
+                #     Min = bm_create_synthetic_random(npl, blockSize, precx, false)
+                # end
+
+                auxs = Vector{AuxDataRGF}()
+                try
+                    # the @time is added only for checking total allocated memory
+                    @time begin
+                        Min = bm_create_synthetic_random(npl, blockSize, precx, false)
                         push!(Mins, Min)
                         push!(Mouts, bm_copy(Min))
+
+                        @time push!(auxs, allocate_aux_data_RGF(Mins[1], parse(Int, ARGS[3]), parse(Int, ARGS[4])))
                     end
-                    auxs = Vector{AuxDataRGF}()
-                    for ix = 1:Threads.nthreads()
-                        push!(auxs, allocate_aux_data_RGF(Mins[ix], parse(Int, ARGS[3]), parse(Int, ARGS[4])))
-                    end
-
-                    # (?) force the garbage collector before doing the core computations
-                    GC.gc()
-
-                    tx(tId) = begin
-                        ninvs = 10
-                        # multiple inversions per energy point, for statistics purposes
-                        for ix = 1:ninvs
-                            # do a clear separation when timing the first inversion vs the others
-                            if ix == 1
-                                timerTagLocal = "thread" * string(tId) * "_first"
-                            else
-                                timerTagLocal = "thread" * string(tId) * "_wo_first"
-                            end
-                            # don't include the first inversion in the flops and mems counting
-                            if useFinerTimings == 1
-                                if ix == 1
-                                    cd = CountingData(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-                                else
-                                    cd = counters[tId]
-                                    cd.nrCalls += 1
-                                end
-                            else
-                                cd = CountingData()
-                            end
-
-                            Printf.@printf(".")
-                            if useFinerTimings == 1
-                                td = TimingData(timers[tId], timerTagLocal)
-                            else
-                                td = TimingData()
-                            end
-                            timerTagLocalTotal = timerTagLocal * "_total"
-                            @timeit timers[tId] timerTagLocalTotal bndiag_of_inv_rgf_local!(Mouts[tId], Mins[tId], auxs[tId], td, cd)
-                        end
-                    end
-
-                    Threads.@threads for ix in 1:Threads.nthreads()
-                        tx(ix)
-                    end
-
-                    Printf.@printf("\n")
+                catch OutOfMemoryError
+                    error("The application tried to allocated beyond the available system memory")
                 end
+
+                # (?) force the garbage collector before doing the core computations
+                GC.gc()
+
+                tx(tId) = begin
+                    ninvs = 10
+                    # multiple inversions per energy point, for statistics purposes
+                    for ix = 1:ninvs
+                        # do a clear separation when timing the first inversion vs the others
+                        if ix == 1
+                            timerTagLocal = "thread" * string(tId) * "_first"
+                        else
+                            timerTagLocal = "thread" * string(tId) * "_wo_first"
+                        end
+                        # don't include the first inversion in the flops and mems counting
+                        if useFinerTimings == 1
+                            if ix == 1
+                                cd = CountingData(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+                            else
+                                cd = counters[tId]
+                                cd.nrCalls += 1
+                            end
+                        else
+                            cd = CountingData()
+                        end
+
+                        Printf.@printf(".")
+                        if useFinerTimings == 1
+                            td = TimingData(timers[tId], timerTagLocal)
+                        else
+                            td = TimingData()
+                        end
+                        timerTagLocalTotal = timerTagLocal * "_total"
+                        @timeit timers[tId] timerTagLocalTotal bndiag_of_inv_rgf_local!(Mouts[tId], Mins[tId], auxs[tId], td, cd)
+                    end
+                end
+
+                tx(1)
+
+                Printf.@printf("\n")
             end
         end
+    end
 
-        for ix = 1:Threads.nthreads()
-            merge!(to, timers[ix], tree_point=[timerTagGlobal])
-        end
+    merge!(to, timers[1], tree_point=[timerTagGlobal])
 
-        # print flops and mems counts for thread1 only
-        if useFinerTimings == 1
-            print_flops_and_mems(counters[1], to, precx, "bndiag_of_inv_ddrgf", true)
-        end
+    # print flops and mems counts for thread1 only
+    if useFinerTimings == 1
+        print_flops_and_mems(counters[1], to, precx, "bndiag_of_inv_ddrgf", true)
     end
 end
 
-LinearAlgebra.BLAS.set_num_threads(1)
+#LinearAlgebra.BLAS.set_num_threads(1)
 
 Printf.@printf("\n")
--- benchmrk/common_to_benchmrks.jl
+++ benchmrk/common_to_benchmrks.jl
@@ -1,24 +1,25 @@
 # 1 from disk, 2 is random
 whereFrom = 2
 # values for the synthetic matrix
-npl = 128
+npl = 720
 blockSize = 64
-# IMPORTANT : the recommended value for nrBlocksInNonPivots is 2, to reduce fill up
-#             as much as possible
-nrBlocksInNonPivots = 3
 
 useFinerTimings = Int(parse(Float64, ARGS[2]))
 
-# load the energy points from an input file
-if whereFrom == 1
-    energVals = LibNEGF.load_energies("3x3")
-else
-    energVals = Dict(50 => -0.16537196040135335, 250 => -0.0918733113340852, 150 => -0.12862263586771927)
-end
-Epoints = Vector{Int}()
-for (key, value) in energVals
-    push!(Epoints, key)
-end
+# enforcing benchmarks, for now, to happen on synthetic data only
+
+# TODO : the following block needs to be restored if we want
+# to go back to using whereFrom = 1
+# # load the energy points from an input file
+# if whereFrom == 1
+#     energVals = LibNEGF.load_energies("3x3")
+# else
+#     energVals = Dict(50 => -0.16537196040135335, 250 => -0.0918733113340852, 150 => -0.12862263586771927)
+# end
+# Epoints = Vector{Int}()
+# for (key, value) in energVals
+#     push!(Epoints, key)
+# end
 
 # list of the precisions to be tested
 # ComplexF16 not fully functional in general
@@ -31,13 +32,14 @@ elseif ARGS[1] == "cpu"
     precs = [ComplexF32, ComplexF64]
 end
 
-# list of systems to loop over
-# do we have to change this test to make use of a different
-# system?
-systemNames = ["3x3"]
+# # list of systems to loop over
+# # do we have to change this test to make use of a different
+# # system?
+# systemNames = ["3x3"]
 
 # list of k points
 # do we want to have more than k=1 in this tests?
 kpoints = [1]
 
-nrEPoints = size(Epoints)[1]
+# nrEPoints = size(Epoints)[1]
+nrEPoints = 1
