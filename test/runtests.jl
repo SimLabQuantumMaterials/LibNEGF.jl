9812674797d071ca6076db2a11d259d563060cff
Gustavo Ramirez-Hidalgo <g.ramirez.hidalgo@fz-juelich.de>
2026-04-11 05:49:09 +0200

    Restore test / blockmatrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ if ARGS[1] == "apple"
 end
 
 # tests common to all of the supported hardwares
-# include("test_blockmatrix.jl")
+include("test_blockmatrix.jl")
 # include("test_backend.jl")
-include("test_matinvert.jl")
+# include("test_matinvert.jl")
 # include("test_keldyshndiag.jl")
--- test/test_blockmatrix.jl
+++ test/test_blockmatrix.jl
@@ -3,46 +3,42 @@ module TestLibNEGFBlockMatrix
 using LibNEGF, Test, SparseArrays
 import LinearAlgebra
 
-# 1 from disk, 2 is random
-whereFrom = 2
+@testset "Blockmatrix" begin
+    @testset "Blockmatrix convert and back" begin
+        # for the data type BlockMatrix, check that converting
+        # to it and back to the sparse format works well
 
-if whereFrom == 1
-    @testset "Blockmatrix" begin
-        @testset "Blockmatrix convert and back" begin
-            # for the data type BlockMatrix, check that converting
-            # to it and back to the sparse format works well
+        include("common_to_test.jl")
 
-            include("common_to_test.jl")
+        for k in kpoints
+            for precx in precs
+                # # list of matrices to load
+                # listMatsToLoad = ["H", "S", "Sc"]
+                # # load in the desired precision
+                # loadedMats, blockSizes = load_matrices(systemx, E, k,
+                #     listMatsToLoad, precx, whereFrom)
+                # H = loadedMats[1]
+                # S = loadedMats[2]
+                # Se = loadedMats[3]
+                # M = build_M_from_HS(H, S, Se, energVals[E])
 
-            for systemx in systemNames
-                for E in Epoints
-                    for k in kpoints
-                        for precx in precs
-                            # list of matrices to load
-                            listMatsToLoad = ["H", "S", "Sc"]
-                            # load in the desired precision
-                            loadedMats, blockSizes = load_matrices(systemx, E, k,
-                                listMatsToLoad, precx, whereFrom)
-                            H = loadedMats[1]
-                            S = loadedMats[2]
-                            Se = loadedMats[3]
-                            M = build_M_from_HS(H, S, Se, energVals[E])
+                # TODO : I think this tests is not making a lot of sense at the moment
 
-                            # convert to BlockMatrix
-                            Abm = bm_convert(M, blockSizes, Dict("in" => 3, "out" => 3))
-                            # convert back to sparse
-                            Asp = bm_convert(Abm)
+                Abm = bm_create_synthetic_random(npl, blockSize, precx, false)
+                M = bm_convert(Abm)
 
-                            relErr = LinearAlgebra.norm(M - Asp, 2) / LinearAlgebra.norm(M, 2)
-                            @test relErr < roundoffs[precx]
-                            @test nnz(Asp) == nnz(M)
-                            @test typeof(Asp) == typeof(M)
-                        end
-                    end
-                end
+                # # convert to BlockMatrix
+                # Abm = bm_convert(M, blockSizes, Dict("in" => 3, "out" => 3))
+
+                # convert back to sparse
+                Asp = bm_convert(Abm)
+
+                relErr = LinearAlgebra.norm(M - Asp, 2) / LinearAlgebra.norm(M, 2)
+                @test relErr < roundoffs[precx]
+                @test nnz(Asp) == nnz(M)
+                @test typeof(Asp) == typeof(M)
             end
         end
     end
 end
-
 end
